841b0ede1b72644a916b6a4b96f3f2411ae3849f
fdonahue <141375104+fdonahue@users.noreply.github.com>
2023-10-28 11:24:09 -0400

    Add files via upload

---
+++ CarOOB.js
@@ -0,0 +1,99 @@
+let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+myFirstVehicle.honk(); // "Beep."
+
+
+class Vehicle {
+    constructor(make, model,year){
+        this.make = make;
+        this.model =model;
+        this.year = year;
+    }
+
+    honk() {
+        return "Beep";
+
+    }
+
+    toString(){
+        return `the vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+    }
+}
+
+
+
+// let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+// myFirstVehicle.toString(); // "The vehicle is a Honda Monster Truck from 1999."
+
+let myFirstCar = new Car("Toyota", "Corolla", 2005);
+myFirstCar.toString(); // "The vehicle is a Toyota Corolla from 2005."
+myFirstCar.honk();     // "Beep."
+myFirstCar.numWheels;  // 4
+
+//extends vehicle constructor
+class Car extends Vehicle{
+    constructor(make,model,year){
+       //super usesconstructor from vehicle
+        super(make,model,year);
+        //adds on numWheels to car
+        this.numWheels = 4;
+    }
+}
+
+
+let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+
+myFirstMotorcycle.toString();
+// "The vehicle is a Honda Nighthawk from 2000."
+
+myFirstMotorcycle.honk();     // "Beep."
+myFirstMotorcycle.revEngine(); // "VROOM!!!"
+myFirstMotorcycle.numWheels;  // 2
+
+class Motorcycle extends Vehicle {
+    constructor(make,model,year){
+        super(make,model,year);
+
+        this.numWheels = 2;
+
+    }
+
+    revEngine(){
+        return "VROOM!!";
+    }
+}
+
+
+let garage = new Garage(2);
+garage.vehicles; // []
+garage.add(new Car("Hyundai", "Elantra", 2015)); // "Vehicle added!"
+garage.vehicles; // [Car]
+garage.add("Taco"); // "Only vehicles are allowed in here!"
+
+garage.add(new Motorcycle("Honda", "Nighthawk", 2000));
+// "Vehicle added!"
+garage.vehicles; // [Car, Motorcycle]
+
+garage.add(new Motorcycle("Honda", "Nighthawk", 2001));
+// "Sorry, we're full."
+
+
+
+class Garage{
+    constructor(capacity){
+        this.vehicles = [];
+        this.capacity= capacity;
+
+    }
+
+    add(newVehicle){
+        if (!(newVehicle instanceof Vehicle)){
+            return " Only Vehicles Allowed!!";
+        }
+        if(this.vehicles.length>=this.capacity){
+            return "Sorry, Garage is full!";
+        }
+        this.vehicles.push(newVehicle);
+        return "Vehicle added to Garage.";
+    }
+}
+
