9e630009e8cbf7a43343b96f8e9ef62c6be5ba65
fdonahue <141375104+fdonahue@users.noreply.github.com>
2024-02-26 00:38:13 -0500

    Add files via upload

---
+++ madlibs/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+
+
+@app.route("/")
+def show_questions():
+    """Return homepage."""
+    prompts = story.prompts
+
+    return render_template("questions.html", prompts=prompts)
+
+@app.route("/story")
+def show_story():
+    """Return homepage."""
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
